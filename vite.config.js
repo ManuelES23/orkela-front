d17b1480d71656767b73ec74b60d491bef418b9f
Manuel <j.mansolesc.223@gmail.com>
2026-09-24 16:55:56 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5175,
+    strictPort: true, // opcional: falla si el puerto ya está en uso, en vez de tomar otro automáticamente
+  },
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
